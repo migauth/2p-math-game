8b0402f04d0cf6ef0b481940ec6c211e9f48a1c2
Michael Gauthier <mikegauthier222@hotmail.com>
2024-04-03 10:30:14 -0700

    add tracking score and exit when a player wins

--- main.rb
+++ main.rb
@@ -14,10 +14,11 @@
 # =end
 
 class Player
-  attr_accessor :name
+  attr_accessor :name, :score
 
   def initialize(name)
     @name = name
+    @score = 0
   end
 end
 
@@ -32,14 +33,17 @@ class Game
     first_num = rand(1..10)
     second_num = rand(1..10)
     real_answer = first_num + second_num
-    puts "#{current_player.name}: What is #{first_num} + #{second_num}? It's #{real_answer}"
+    puts "#{current_player.name}: What is #{first_num} + #{second_num}?"
     print "> "
     user_answer = $stdin.gets.chomp.to_i
     
     if user_answer == real_answer
+      current_player.score += 1
       puts "Correct!"
+      puts "Score: Player1 #{@player1.score}/3 | Player2 #{@player2.score}/3 "
     else
       puts 'WRONG'
+      puts "Score: Player1 #{@player1.score}/3 | Player2 #{@player2.score}/3 "
     end
   end
 
@@ -50,7 +54,12 @@ class Game
   def start_game
     loop do
       question(@current_player)
+      if @player1.score == 3 || @player2.score == 3
+        puts "#{@current_player.name} wins!"
+        exit(0)
+      else
       switch_players
+      end
     end
   end
 end
